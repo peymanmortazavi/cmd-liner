bf63f46e9631315a1598d878d193fc34a5cc22cf
Peyman Mortazavi <pmortazavi@cailmobility.com>
2015-05-18 15:27:53 -0700

    stability improvements

--- cmd-liner.js
+++ cmd-liner.js
@@ -3,13 +3,15 @@ var colors = require('colors');     // Pretty colors
 
 exports.init = function(commands, options, action) {
   
+  cmd.usage(generateUsage(commands));
+  
   options = options || { };
   action = action || function() { console.log(cmd.help()) }
   
   var argv = cmd.argv;
   
   if(argv._.length < 1) {
-    action(argv);
+    action(cmd);
     return;
   }
   
@@ -18,13 +20,16 @@ exports.init = function(commands, options, action) {
     
     var currentHandler = argv._[i];
 
-    if(currentCommand.commands[currentHandler]) {
+    if(currentCommand.commands && currentCommand.commands[currentHandler]) {
       
       if(i == argv._.length-1){
-        currentCommand.commands[currentHandler].action(argv);
+        currentCommand.commands[currentHandler].action = 
+          currentCommand.commands[currentHandler].action || function(){console.log(cmd.help());}
+        currentCommand.commands[currentHandler].action(cmd);
         break;
       } else {
         currentCommand = currentCommand.commands[currentHandler];
+        cmd.usage(generateUsage(currentCommand.commands));
         continue;
       }
     }
@@ -33,9 +38,21 @@ exports.init = function(commands, options, action) {
       console.log("ERROR:".inverse + " Could not find handler for " + currentHandler); process.exit(1); 
     }
     
-    currentCommand.nohandler(argv);
+    currentCommand.nohandler(cmd);
     break;
     
   }
+  
+}
+
+function generateUsage(commands) {
+  
+  var usage = "Usage: $0 [COMMAND]\n\nAvailable Commands:";
+  
+  for(var commandName in commands) {
+    usage += '\n   '+commandName + '\t\t'+(commands[commandName].desc || '');
+  }
+  
+  return usage;
   
 }
